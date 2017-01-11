a08e124634f9a2a036fde63be8df2c5d859c1308
Karl Marques <kmarques@hi-media.com>
2017-01-11 15:14:30 +0100

    [FIX] ini dump with array values

--- Executor/IniExecutor.php
+++ Executor/IniExecutor.php
@@ -30,21 +30,34 @@ class IniExecutor implements ExecutorInterface
             $string .= "[$section]" . PHP_EOL;
             foreach ($array as $key => $value) {
                 switch (true) {
-                    case is_numeric($value):
+                    case is_array($value):
+                        foreach ($value as $subKey => $subValue) {
+                            $string .= $key . "[" . $subKey . "] = " . $this->formatValues($subValue) . PHP_EOL;
+                        }
                         break;
-                    case is_bool($value):
-                        $value = $value ? 1 : 0;
-                        break;
-                    case is_string($value):
-                        $value = '"' . $value . '"';
+                    default:
+                        $string .= $key . " = " . $this->formatValues($value) . PHP_EOL;
                         break;
                 }
-                $string .= $key . " = " . $value . PHP_EOL;
             }
             $string .= PHP_EOL;
         }
 
         return trim($string, PHP_EOL);
+    }
+
+    private function formatValues($value)
+    {
+        switch (true) {
+            case is_bool($value):
+                return ($value ? 1 : 0);
+                break;
+            case is_string($value):
+                return '"' . $value . '"';
+                break;
+        }
+
+        return $value;
     }
 
     /**
